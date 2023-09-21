d8f52775c1645ef30cd16b21a98417102b70b113
HermanLui-IJS <79297153+HermanLui-IJS@users.noreply.github.com>
2023-09-21 07:54:47 -0400

    Added api

---
+++ src/api.ts
@@ -0,0 +1,178 @@
+import { Wallet, BigNumber } from "@ijstech/eth-wallet";
+import { Contracts } from "@scom/oswap-openswap-contract";
+
+const nativeTokenDecimals = 18;
+
+export async function doAddLiquidity(
+    wallet:Wallet,
+    liqProvider:string,
+    tokenA: string,
+    tokenB: string,
+    tokenInDecimals: number,
+    addingTokenA: boolean,
+    pairIndex: number | BigNumber,
+    offerIndex: number | BigNumber,
+    amountIn: number | BigNumber,
+    allowAll: boolean,
+    restrictedPrice: number | BigNumber,
+    startDate: number | BigNumber,
+    expire: number | BigNumber,
+    deadline: number | BigNumber,
+) {
+    let amountInWei = new BigNumber(amountIn).shiftedBy(tokenInDecimals);
+    let liqProviderContracts = new Contracts.OSWAP_RestrictedLiquidityProvider1(wallet,liqProvider);
+    let receipt = await liqProviderContracts.addLiquidity({
+        tokenA, 
+        tokenB, 
+        addingTokenA, 
+        pairIndex,
+        offerIndex,
+        amountIn:amountInWei,
+        allowAll,
+        restrictedPrice,
+        startDate,
+        expire,
+        deadline,
+    });
+    return receipt;
+}
+
+export async function doAddLiquidityETH(
+    wallet:Wallet,
+    liqProvider:string,
+    tokenA: string,
+    tokenADecimals: number,
+    addingTokenA: boolean,
+    pairIndex: number | BigNumber,
+    offerIndex: number | BigNumber,
+    amountIn: number | BigNumber,
+    allowAll: boolean,
+    restrictedPrice: number | BigNumber,
+    startDate: number | BigNumber,
+    expire: number | BigNumber,
+    deadline: number | BigNumber,
+) {
+    let ethAmount = new BigNumber("0");
+    let amountAIn = new BigNumber("0");
+    if (addingTokenA) {
+        amountAIn = new BigNumber(amountIn).shiftedBy(tokenADecimals);
+    } else {
+        ethAmount = new BigNumber(amountIn).shiftedBy(nativeTokenDecimals);
+    }
+    let liqProviderContracts = new Contracts.OSWAP_RestrictedLiquidityProvider1(wallet,liqProvider);
+    let receipt = await liqProviderContracts.addLiquidityETH({
+        tokenA, 
+        addingTokenA, 
+        pairIndex,
+        offerIndex,
+        amountAIn,
+        allowAll,
+        restrictedPrice,
+        startDate,
+        expire,
+        deadline,
+    }, ethAmount);
+    return receipt;
+}
+
+export async function doAddLiquidityAndTrader(
+    wallet:Wallet,
+    liqProvider:string,
+    tokenA: string,
+    tokenB: string,
+    tokenInDecimals: number,
+    addingTokenA: boolean,
+    pairIndex: number | BigNumber,
+    offerIndex: number | BigNumber,
+    amountIn: number | BigNumber,
+    allowAll: boolean,
+    restrictedPrice: number | BigNumber,
+    startDate: number | BigNumber,
+    expire: number | BigNumber,
+    deadline: number | BigNumber,
+    whitelistAddress:[{address:string,allocation:BigNumber,oldAllocation:BigNumber}]
+) {
+    let trader: string[] = [];
+    let allocation: BigNumber[] = [];
+    whitelistAddress.map(v => {
+        if (!v.allocation.eq(v.oldAllocation)) {
+          trader.push(v.address);
+          allocation.push(v.allocation.shiftedBy(tokenInDecimals));
+        }
+    });
+    let amountInWei = new BigNumber(amountIn).shiftedBy(tokenInDecimals);
+    let liqProviderContracts = new Contracts.OSWAP_RestrictedLiquidityProvider1(wallet,liqProvider);
+    let receipt = await liqProviderContracts.addLiquidityAndTrader({
+        param:[
+            new BigNumber(tokenA.toLowerCase()),
+            new BigNumber(tokenB.toLowerCase()),
+            addingTokenA?1:0,
+            pairIndex,
+            offerIndex,
+            amountInWei,
+            allowAll?1:0,
+            restrictedPrice,
+            startDate,
+            expire,
+            deadline
+        ],
+    trader,
+    allocation
+    });
+    
+    return receipt;
+}
+
+export async function doAddLiquidityETHAndTrader(
+    wallet:Wallet,
+    liqProvider:string,
+    tokenA: string,
+    tokenADecimals: number,
+    addingTokenA: boolean,
+    pairIndex: number | BigNumber,
+    offerIndex: number | BigNumber,
+    amountIn: number | BigNumber,
+    allowAll: boolean,
+    restrictedPrice: number | BigNumber,
+    startDate: number | BigNumber,
+    expire: number | BigNumber,
+    deadline: number | BigNumber,
+    whitelistAddress:[{address:string,allocation:BigNumber,oldAllocation:BigNumber}]
+) {
+    let tokenInDecimals = nativeTokenDecimals;
+    let ethAmount = new BigNumber("0");
+    let amountAIn = new BigNumber("0");
+    if (addingTokenA) {
+        amountAIn = new BigNumber(amountIn).shiftedBy(tokenADecimals);
+        tokenInDecimals = tokenADecimals;
+    } else {
+        ethAmount = new BigNumber(amountIn).shiftedBy(nativeTokenDecimals);
+    }
+    let trader: string[] = [];
+    let allocation: BigNumber[] = [];
+    whitelistAddress.map(v => {
+        if (!v.allocation.eq(v.oldAllocation)) {
+          trader.push(v.address);
+          allocation.push(v.allocation.shiftedBy(tokenInDecimals));
+        }
+    });
+    let liqProviderContracts = new Contracts.OSWAP_RestrictedLiquidityProvider1(wallet,liqProvider);
+    let receipt = await liqProviderContracts.addLiquidityETHAndTrader({
+        param:[
+            new BigNumber(tokenA.toLowerCase()),
+            addingTokenA?1:0,
+            pairIndex,
+            offerIndex,
+            amountAIn,
+            allowAll?1:0,
+            restrictedPrice,
+            startDate,
+            expire,
+            deadline
+        ],
+    trader,
+    allocation
+    },ethAmount);
+    
+    return receipt;
+}
